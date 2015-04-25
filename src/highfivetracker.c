03b8c72ffe9cb38282d924e7918cfba22495befd
thegrumpybuffalo <thegrumpybuffalo@gmail.com>
2015-04-25 18:17:55 -0500

    Functioning but buggy high five sensor

--- src/highfivetracker.c
+++ src/highfivetracker.c
@@ -10,7 +10,9 @@
 #define NUM_HIGHFIVES_DEFAULT 0
 
 static Window *s_main_window;
-double highestFive=0;
+long long highestFive=0;
+long long lastFive=0;
+static long long averageFive=2000;
 double hx;
 double hy;
 double hz;
@@ -29,8 +31,7 @@ double hz;
   }
 }
 */
-static char acc_buffer[128];
-static int max(int x, int y , int z){
+static long long max(long long x, long long y , long long z){
   if (x<y){
     if (z<y){
       return y;
@@ -46,18 +47,6 @@ static int max(int x, int y , int z){
   return z;
 }
 static TextLayer *s_acc_layer;
-static void data_handler(AccelData *data, uint32_t num_samples) {
-  int mx,my,mz;
-  mx=max(data[0].x,data[1].x,data[2].x);
-  my=max(data[0].y,data[1].y,data[2].y);
-  mz=max(data[0].z,data[1].z,data[2].z);
-  int temp=max(mx,my,mz);
-  if (temp>highestFive)
-    highestFive=temp;
-  static char acc_text[18];
-  snprintf(acc_text, sizeof(acc_text), "%d", (int)highestFive);
-  text_layer_set_text(s_acc_layer, acc_text);;
-}
 
 static ActionBarLayer *s_action_bar;
 static TextLayer *s_header_layer, *s_body_layer, *s_label_layer;
@@ -69,6 +58,35 @@ static void update_text() {
   static char s_body_text[18];
   snprintf(s_body_text, sizeof(s_body_text), "%u High Fives", s_num_fives);
   text_layer_set_text(s_body_layer, s_body_text);
+}
+
+static void data_handler(AccelData *data, uint32_t num_samples) {
+  long long mx,my,mz;
+  mx=max(data[0].x,data[1].x,data[2].x);
+  my=max(data[0].y,data[1].y,data[2].y);
+  mz=max(data[0].z,data[1].z,data[2].z);
+  int acceleration_2 = data[0].x * data[0].x + data[0].y * data[0].y + data[0].z * data[0].z;
+  int acceleration = acceleration_2;
+  int next = (acceleration + acceleration_2 / acceleration) / 2;
+  while (acceleration != next) {
+    acceleration = next;
+    next = (acceleration + acceleration_2 / acceleration) / 2;
+  }
+  if (acceleration>highestFive)
+    highestFive=acceleration;
+  long long temp=max(mx,my,mz);
+  if (temp>highestFive)
+    highestFive=temp;
+  static char acc_text[32];
+  lastFive=acceleration;
+  snprintf(acc_text, sizeof(acc_text), "%5d", (int)highestFive);
+  if (lastFive>averageFive){
+    snprintf(acc_text, sizeof(acc_text), "Nice Five!");
+      s_num_fives++;
+      update_text();
+      vibes_short_pulse();
+  }
+  text_layer_set_text(s_acc_layer, acc_text);;
 }
 
 static void increment_click_handler(ClickRecognizerRef recognizer, void *context) {
@@ -93,10 +111,6 @@ static void click_config_provider(void *context) {
 static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   
-  
- 
-  
-  
   s_action_bar = action_bar_layer_create();
   action_bar_layer_add_to_window(s_action_bar, window);
   action_bar_layer_set_click_config_provider(s_action_bar, click_config_provider);
@@ -112,21 +126,21 @@ static void main_window_load(Window *window) {
   text_layer_set_text(s_header_layer, "High Five Counter");
   layer_add_child(window_layer, text_layer_get_layer(s_header_layer));
 
-  s_body_layer = text_layer_create(GRect(4, 36, width, 60));
-  text_layer_set_font(s_body_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
-  text_layer_set_background_color(s_body_layer, GColorClear);
-  text_layer_set_text(s_body_layer, "highest five");
-  layer_add_child(window_layer, text_layer_get_layer(s_body_layer));
+  s_header_layer = text_layer_create(GRect(4, 36, width, 60));
+  text_layer_set_font(s_header_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+  text_layer_set_background_color(s_header_layer, GColorClear);
+  text_layer_set_text(s_header_layer, "highest five");
+  layer_add_child(window_layer, text_layer_get_layer(s_header_layer));
  
-   s_acc_layer = text_layer_create(GRect(4, 70, width, 60));
+   s_acc_layer = text_layer_create(GRect(4, 70, width-5, 60));
   text_layer_set_font(s_acc_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
   text_layer_set_text(s_acc_layer, "No data yet.");
-  text_layer_set_overflow_mode(s_acc_layer, GTextOverflowModeWordWrap);
+  //text_layer_set_overflow_mode(s_acc_layer, GTextOverflowModeWordWrap);
   layer_add_child(window_layer, text_layer_get_layer(s_acc_layer));
       
   
   s_body_layer = text_layer_create(GRect(4, 115, width, 60));
-   text_layer_set_font(s_body_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_font(s_body_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
   text_layer_set_background_color(s_body_layer, GColorClear);
   layer_add_child(window_layer, text_layer_get_layer(s_body_layer));
   update_text();
@@ -138,20 +152,6 @@ static void main_window_unload(Window *window) {
   text_layer_destroy(s_label_layer);
   text_layer_destroy(s_acc_layer);
   action_bar_layer_destroy(s_action_bar);
-}
-
-static void timer_callback(void *data) {
-  AccelData accel = (AccelData) { .x = 0, .y = 0, .z = 0 };
-  accel_service_peek(&accel);
-  hx=accel.x;
-  hy=accel.y;
-  if (hx>highestFive || hy>highestFive){
-  if (hx<hy)
-    highestFive=hx;
-  else
-    highestFive=hy;
-  }
-  app_timer_register(ACCEL_STEP_MS, timer_callback, NULL);
 }
 
 
