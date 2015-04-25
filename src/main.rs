1b9fb3d0dc6a93464790d29fc4633669deee6fdd
Hawken Rives <hawkrives@gmail.com>
2015-04-25 00:43:32 -0500

    add rust port of algorithms code from today

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,30 @@
-// This code is editable and runnable!
+fn can_make_change(denominations: &[i32], value: i32) -> bool {
+	let max = (value + 1) as usize;
+
+	let mut change = vec![false; max];
+	change[0] = true;
+
+	for val in 1 .. (value + 1) {
+	    for d in denominations {
+	    	let idx = val - d;
+	        if idx >= 0 && change[idx as usize] {
+	            change[val as usize] = true;
+	        }
+	    }
+	}
+
+	change[value as usize]
+}
+
 fn main() {
-    // A simple integer calculator:
-    // `+` or `-` means add or subtract by 1
-    // `*` or `/` means multiply or divide by 2
+	let denominations = vec![17, 10];
+	let value = 117;
+	let change_p = can_make_change(&denominations, value);
 
-    let program = "+ + * - /";
-    let mut accumulator = 0;
-
-    for token in program.chars() {
-        match token {
-            '+' => accumulator += 1,
-            '-' => accumulator -= 1,
-            '*' => accumulator *= 2,
-            '/' => accumulator /= 2,
-            _ => { /* ignore everything else */ }
-        }
-    }
-
-    println!("The program \"{}\" calculates the value {}",
-              program, accumulator);
+	if change_p {
+		println!("can make change!");
+	}
+	else {
+		println!("no change can be made...");
+	}
 }
